17840d178128f55a7b6ca614a6acdc2ab5d3e8d7
mauricio.nobrega <mauricio.nobrega@netshoes.com>
2016-09-12 21:39:49 -0300

    Initiated gulp and module pattern for components

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    scripts = [
+      'src/js/vendor/react.js',
+      'src/js/vendor/react-dom.js',
+      'src/js/vendor/browser.js',
+      'src/js/vendor/InputElement.js'
+    ];
+
+gulp.task('scripts', function() {
+  gulp.src(scripts).pipe(concat('react.js')).on('error', function(error){
+      console.log(error);
+    }).pipe(gulp.dest('build'))
+    .pipe(rename({
+      suffix: '.min'
+    })).on('error', function(error){
+      console.log(error);
+    })
+    .pipe(uglify()).on('error', function(error){
+      console.log(error);
+    }).pipe(gulp.dest('build'));
+});
+
+gulp.task('copyApp', function() {
+  gulp.src('src/js/app.js').pipe(gulp.dest('build'));
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,60 +1,68 @@
-var Title = React.createClass({
- render: function() {
-   return (
-     <h1>{this.props.text}</h1>
-   );
- }
-});
+var app = (function ($, win, doc) {
+  'use strict';
 
-var Input = React.createClass({
-  handleChange: function(event) {
-    this.setState({value: event.target.value.substr(0, 9)});
-  },
-  render: function() {
-    return (
-      <input
-      type='text'
-      id={'input-' + this.props.name.toLowerCase()}
-      name={'input-' + this.props.name.toLowerCase()}
-      placeholder='00000-000'
-      pattern='[0-9]{5}[\-]?[0-9]{3}'
-      onChange={this.handleChange} />
+  function init() {
+    var Title = React.createClass({
+     render: function() {
+       return (
+         <h1>{this.props.text}</h1>
+       );
+     }
+    });
+
+    var Input = React.createClass({
+      render: function() {
+        return (
+          <InputElement
+            type='text'
+            id={'input-' + this.props.name.toLowerCase()}
+            name={'input-' + this.props.name.toLowerCase()}
+            pattern='[0-9]{5}[\-]?[0-9]{3}'
+            mask='99999-999'
+            placeholder='00000-000' />
+        );
+      }
+    });
+
+    var Label = React.createClass({
+      render: function() {
+        return (
+          <label htmlFor={'input-' + this.props.name.toLowerCase()}>
+            {this.props.name}
+            <Input name={this.props.name} />
+          </label>
+        );
+      }
+    });
+
+    var Button = React.createClass({
+      render: function() {
+        return (
+          <button type='submit'>{this.props.text}</button>
+        );
+      }
+    });
+
+    var SearchBar = React.createClass({
+      render: function() {
+        return (
+          <header>
+            <Title text={this.props.title} />
+            <Label name={this.props.name} />
+            <Button name={this.props.name} text={this.props.buttonText} />
+          </header>
+        );
+      }
+    });
+
+    ReactDOM.render(
+      <SearchBar name='CEP' title='Consultar' buttonText='Buscar' />,
+      document.getElementById('zipcode')
     );
-  }
-});
+  };
 
-var Label = React.createClass({
-  render: function() {
-    return (
-      <label htmlFor={'input-' + this.props.name.toLowerCase()}>
-        {this.props.name}
-        <Input name={this.props.name} />
-      </label>
-    );
+  return {
+    'init': init
   }
-});
 
-var Button = React.createClass({
-  render: function() {
-    return (
-      <button type='submit'>{this.props.text}</button>
-    );
-  }
-});
-
-var SearchBar = React.createClass({
-  render: function() {
-    return (
-      <header>
-        <Title text={this.props.title} />
-        <Label name={this.props.name} />
-        <Button name={this.props.name} text={this.props.buttonText} />
-      </header>
-    );
-  }
-});
-
-ReactDOM.render(
-  <SearchBar name='CEP' title='Consultar' buttonText='Buscar' />,
-  document.getElementById('zipcode')
-);
+}(undefined, window, window.document)).init();
